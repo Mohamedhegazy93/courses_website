95922de78ca5e79c984af224379a422730ffb537
unknown <mohamedhegazy49644@gmail.com>
2025-02-26 17:00:17 +0200

    commit

--- server.js
+++ server.js
@@ -4,11 +4,16 @@ dotenv.config();
 import cookieParser from 'cookie-parser'
 
 import {connectDB} from './config/dbConnection.js'
+import cors from 'cors'
+import compression from "compression";
 
 
 
 connectDB()
 const app = express();
+app.use(cors())
+app.options('*',cors())
+app.use(compression())
 app.use(cookieParser());
 
 app.use(express.json());
@@ -18,6 +23,7 @@ import courseRoute from './routes/course.route.js'
 import orderRoute from './routes/order.route.js'
 import globalError from './middlewares/error.middleware.js'
 import ApiError from './utils/apiError.js'
+import compression from "compression";
 
 
 app.use('/v1/api/auth',authRoute)
